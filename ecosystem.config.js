f217f8c2d93f12146e21107aeb560620f9e0e662
APorechniy <porechniy@list.ru>
2026-07-30 00:09:31 +0500

    Fix ecoconfig

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,13 +3,8 @@ module.exports = {
         name: 'quizer',
         script: 'npm',
         args: 'run dev',
-        instances: 1,
         autorestart: true,
         watch: false,
-        max_memory_restart: '500M',
-        log_date_format: 'YYYY-MM-DD HH:mm:ss',
-        error_file: './logs/error.log',
-        out_file: './logs/out.log',
-        merge_logs: true
+        max_memory_restart: '500M'
     }]
 };
