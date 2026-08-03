ebd01b28468f026b6e6e3f836753e97aca3d1fc0
APorechniy <porechniy@list.ru>
2026-08-03 17:40:25 +0500

    Fix errors and timers

--- src/commands/send_question.ts
+++ src/commands/send_question.ts
@@ -7,7 +7,7 @@ import { getSession } from "../storage";
 // Отправка вопроса пользователю
 export async function sendQuestion(ctx: Context, userId: number, questionIndex: number): Promise<void> {
     const session = await getSession(userId);
-
+    console.log("CurrentQuizetionIndex:", questionIndex)
     if (questionIndex < QUESTIONS.length) {
         const question = QUESTIONS[questionIndex];
 
@@ -33,7 +33,7 @@ export async function sendQuestion(ctx: Context, userId: number, questionIndex: 
     } else {
         // Переходим к сбору контактов
         session.state = QuizState.WAITING_FOR_CONTACT;
-
+        console.log("WAIT FOR CONTACTS",)
         const contactText =
             "Спасибо за ваши ответы! 🎉\n" +
             "Пожалуйста, оставьте ваш телефон или email, чтобы мы могли связаться с вами.\n\n" +
